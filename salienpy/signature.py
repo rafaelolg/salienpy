f59bb6258414978efecc880c492f5b1346f916af
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-08-01 14:31:03 -0300

    make it ready for odd sized images

--- salienpy/signature.py
+++ salienpy/signature.py
@@ -10,6 +10,8 @@ def signature_saliency(img):
     X. Hou, J. Harel, and C. Koch, "Image Signature: Highlighting Sparse Salient
     Regions." IEEE Trans. Pattern Anal. Mach. Intell. 34(1): 194-201 (2012)
     """
+    old_shape = (img.shape[0],img.shape[1])
+    img = img_padded_for_dct(img)
     img = img/255.0
     sal = []
     for c in range(img.shape[2]):
@@ -19,6 +21,18 @@ def signature_saliency(img):
         s = np.multiply(s,s)
         sal.append(s)
     sal = sum(sal)/3.0
-    sal = sal - sal.min()
-    sal = 255 * ( sal / sal.max())
+    #sal = sal[:old_shape[0], :old_shape[1]]
+    #sal = sal - sal.min()
+    #sal = 255 * ( sal / sal.max())
     return sal
+
+def img_padded_for_dct(img):
+    h = img.shape[0]
+    w = img.shape[1]
+    if (h%2 == 1):
+        h=h+1
+    if (w%2 == 1):
+        w=w+1
+    return cv2.copyMakeBorder(img, top=0,  bottom=(h-img.shape[0]),
+                                   left=0, right=(w-img.shape[1]),
+                                   borderType=cv2.BORDER_REPLICATE)
