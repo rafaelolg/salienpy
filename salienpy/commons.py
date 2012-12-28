084d3013c57dc906ea1bdb231d5ba1ecd97c7b6e
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-12-28 03:32:13 -0200

    docs

--- salienpy/commons.py
+++ salienpy/commons.py
@@ -1,6 +1,7 @@
 def minmaxnormalization(vector):
     """
     Makes the min max normalization over a numpy vector
+    $$ v_i = (v_i / max(v)) - (min((v_i)/max(v_i))
     """
     vector = vector / (vector.max())
     vector = vector - (vector.min())
--- salienpy/frequency_tuned.py
+++ salienpy/frequency_tuned.py
@@ -3,33 +3,47 @@ import cv2
 import numpy
 from commons import minmaxnormalization
 
-def frequency_tuned_saliency(img):
+
+def frequency_tuned_saliency(image):
     """
     Frequency Tuned Saliency.
-    Find the Euclidean distance between the Lab pixel vector in a Gaussian filtered image
+    Find the Euclidean distance between
+    the Lab pixel vector in a Gaussian filtered image
     with the average Lab vector for the input image.
-    R. Achanta, S. Hemami, F. Estrada and S. Susstrunk, Frequency-tuned Salient Region
-    Detection, IEEE International Conference on Computer Vision and Pattern Recognition
+    R. Achanta, S. Hemami, F. Estrada and S. Susstrunk,
+    Frequency-tuned Salient Region
+    Detection, IEEE International Conference on Computer
+     Vision and Pattern Recognition
 
     Args:
-        img (numpy.array): an image.
+        image (numpy.array): an image.
 
     Returns:
        a 2d image saliency map.
     """
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
     #mean of each channel
     means = []
-    for c in range(img.shape[2]):
-        means.append(img[:,:,c].mean())
+    for c in range(image.shape[2]):
+        means.append(image[:, :, c].mean())
     means = numpy.asarray(means)
 
-    img = cv2.medianBlur(img, 9)
-    dist = (img - means)**2
-    print("mean color is %s"% means)
+    image = cv2.medianBlur(image, 9)
+    dist = (image - means) ** 2
+    print("mean color is %s" % means)
     salmap = numpy.zeros((dist.shape[0], dist.shape[1]))
     for i in range(dist.shape[0]):
         for j in range(dist.shape[1]):
             salmap[i][j] = numpy.sqrt(dist[i][j].sum())
     return minmaxnormalization(salmap)
 
+if __name__ == '__main__':
+    import sys
+    import re
+    import os
+    img = cv2.imread(sys.argv[1])
+    basename = os.path.basename(sys.argv[1])
+    m = re.search('(.*)\..+', basename)
+    stem = m.groups()[0]
+    sal = frequency_tuned_saliency(img) * 255
+    cv2.imwrite("%s.png" % stem, sal)
