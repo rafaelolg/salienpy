754fa121a423aa546e8e610b069f1f3a2a6eae5c
Rafael Lopes <rafaellg@vision.ime.usp.br>
2013-01-03 23:09:17 -0200

    Parece que o kmeans ele resolve mais de boa.

--- salienpy/dictionary_frequency.py
+++ salienpy/dictionary_frequency.py
@@ -1,27 +1,29 @@
 import numpy
 from sklearn.feature_extraction.image import extract_patches_2d
-from sklearn.decomposition import MiniBatchDictionaryLearning
-from sklearn.decomposition import FastICA
-from sklearn.cluster import KMeans
+#from sklearn.decomposition import MiniBatchDictionaryLearning
+#from sklearn.decomposition import FastICA
+from sklearn.cluster import MiniBatchKMeans
 from matplotlib import pyplot
 
-def extract_dictionary(image, patches_size=(12,12), projection_dimensios=24, previous_dictionary=None):
+def extract_dictionary(image, patches_size=(12,12), projection_dimensios=12, previous_dictionary=None):
     """
     Gets a higher dimension ica projection image.
 
     """
-    patches = extract_patches_2d(image, patches_size, max_patches=0.7)
+    patches = extract_patches_2d(image, patches_size, max_patches=200000)
     patches = numpy.reshape(patches, (patches.shape[0],-1))
-    patches = (patches - patches.mean(axis=0))# / numpy.std(patches, axis=0)
+    #patches = (patches - patches.mean(axis=0))# / numpy.std(patches, axis=0)
     print "training patches shape = %s"% str(patches.shape)
-    dico = MiniBatchDictionaryLearning(n_atoms=projection_dimensios, alpha=1, n_iter=100, transform_algorithm='threshold')
-    fit = dico.fit(patches)
-    return fit
+    if not previous_dictionary:
+        previous_dictionary = 'k-means++'
+    kmeans  = MiniBatchKMeans(projection_dimensios,compute_labels=False,init=previous_dictionary)
+    kmeans.fit(patches)
+    return kmeans.cluster_centers_
 
 def image_to_dictionary_space(image, dictionary, patches_size=(12,12)):
     patches = extract_patches_2d(image, patches_size)
     patches = numpy.reshape(patches, (patches.shape[0],-1))
-    patches = (patches - patches.mean(axis=0))# / numpy.std(patches, axis=0)
+    #patches = (patches - patches.mean(axis=0))# / numpy.std(patches, axis=0)
     dict_encoded = dictionary.transform(patches)
     print "encoding patches shape = %s"% str(patches.shape)
     return dict_encoded
@@ -30,13 +32,16 @@ if __name__ == '__main__':
     from  cv2 import imread, imwrite
     import sys
     image = imread(sys.argv[1])
-    d = extract_dictionary(image)
-    encoded = image_to_dictionary_space(image, d)
-
-    components  = d.components_ -  d.components_.min()
-    components  = 255* (components/components.max())
+    components = extract_dictionary(image)
+    #encoded = image_to_dictionary_space(image, d)
+    #components  = d -  d.min()
+    #components  = 255* (components/components.max())
+    fig = pyplot.figure()
     components = components.astype('uint8')
+    import matplotlib
+    matplotlib.use('Agg')
     for i, c in enumerate(components):
-        pyplot.subplot(12, 12, i + 1)
-        c = c.reshape((12,12,3)) 
-        pyplot.imshow(c)
+        ax = fig.add_subplot(12, 2, i + 1)
+        c = c.reshape((12,12,3))
+        ax.imshow(c)
+    pyplot.savefig('out.png')
