8dff15373dcbb56ea265712badf6109cf3d05a54
Rafael Lopes <rafaellg@vision.ime.usp.br>
2013-01-07 00:22:42 -0200

    Also shows the aws saliency

--- view_saliency.py
+++ view_saliency.py
@@ -5,22 +5,24 @@ import sys
 import salienpy.frequency_tuned
 import salienpy.signature
 import salienpy.kmeans_frequency
+import salienpy.aws
 
 
 def main(img):
-    cv2.imshow('Original Image', img)
     saliency_methods = [
                 ('frequency_tuned',
                     salienpy.frequency_tuned.frequency_tuned_saliency),
+                ('aws',
+                    salienpy.aws.aws_saliency),
                 ('signature',
                     salienpy.signature.signature_saliency),
                 ('kmeans_frequency',
-                        salienpy.kmeans_frequency.kmeans_frequency_saliency)]
+                        salienpy.kmeans_frequency.kmeans_frequency_tuned_saliency)]
 
     for name, method in saliency_methods:
-        sal_img = method(img)
-        cv2.imshow(name, sal_img)
-        cv2.imwrite(name + '.png', sal_img)
+        print name
+        sal_img = method(img.copy())
+        cv2.imwrite(name + '.png', 255 * sal_img.astype('uint8'))
     cv2.waitKey()
 
 
