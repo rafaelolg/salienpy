8e9e0beee10e52862a149e5b0ea9df084d066a2b
Rafael Lopes <rafael@whitehat.com.br>
2013-03-04 08:40:54 -0300

    compara multiplas imagens.

--- compare_imgs.py
+++ compare_imgs.py
@@ -9,7 +9,7 @@ import salienpy.frequency_tuned
 import salienpy.signature
 import salienpy.dictionary_frequency
 import salienpy.comparison
-
+from salienpy.commons import minmaxnormalization
 def main(img_a, img_b):
     saliency_methods = [ ('dictionary_ica_saliency', salienpy.dictionary_frequency.dictionary_saliency),
                        ]
@@ -18,17 +18,23 @@ def main(img_a, img_b):
         t = time()
         sal_a = method(img_a.copy())
         sal_b = method(img_b.copy())
-        corr = salienpy.comparison.compare_saliencies(sal_a,sal_b)
         t = t - time()
-        return corr
+        print 'time = %s'
+
+        return sal_a , sal_b
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 5:
         img_a  = cv2.imread(sys.argv[1])
         img_b  = cv2.imread(sys.argv[2])
-        corr = main(img_a, img_b)
-        print '%s <br> ![original](%s) ![protan](%s)'%(corr, sys.argv[1], sys.argv[2])
-
+        sal_a, sal_b = main(img_a, img_b)
+        cv2.imwrite(sys.argv[3],(255 * minmaxnormalization(sal_a)).astype('uint8'))
+        cv2.imwrite(sys.argv[4],(255 * minmaxnormalization(sal_b)).astype('uint8'))
+        cv2.imwrite(sys.argv[5],(255 * minmaxnormalization(sal_a - sal_b)).astype('uint8'))
+        print '%s\t%s\t%s\t%s\t%s' % sys.argv[1:6]
+    else:
+        import pprint
+        pprint.pprint(sys.argv)
 
 # compare_imgs.py
